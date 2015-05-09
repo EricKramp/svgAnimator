620b65add16c5bfe4c73fb1c872c4380b5f4ac39
Eric Kramp <mail@erickramp.com>
2015-05-09 15:48:30 -0400

    cleaned up print statements

--- js/animationRecorder.js
+++ js/animationRecorder.js
@@ -14,36 +14,28 @@ var AnimationRecorder = (function() {
 	};
 
 	publicAPI.record = function() {
-		console.log("starting recording");
 		recordIntv = setInterval(recordMotion, intervalDuration);
 	};
 
 	publicAPI.stopRecording = function() {
-		console.log("stopping recording");
 		clearInterval(recordIntv);
 	};
 
 	publicAPI.playRecording = function() {
-		console.log("playing recording");
-		// playIdx = 0;
 		playIntv = setInterval(playMotion, intervalDuration);
 	};
 
 	publicAPI.stopPlayback = function() {
-		console.log("stopping playback");
 		clearInterval(playIntv);
 	};
 
 	// private methods
 
 	function recordMotion () {
-		console.log("adding motion point");
 		motion.push(motionTarget.transform().localMatrix);
-		console.log (motion[motion.length-1]);
 	}
 
 	function playMotion () {
-		console.log("playMotion: " + motion[playIdx]);
 		motionTarget.transform(motion[playIdx]);
 		playIdx ++;
 		if (playIdx > motion.length) {
