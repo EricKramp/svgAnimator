b55bdfdafaee920ffbad9e81626d16a0807e2f03
Eric Kramp <mail@erickramp.com>
2015-05-09 15:37:13 -0400

    wooo, basic animation recording

--- js/animationRecorder.js
+++ js/animationRecorder.js
@@ -4,24 +4,52 @@ var AnimationRecorder = (function() {
 	var publicAPI = {},
 	motion = [],
 	motionTarget,
-	interval;
+	recordIntv,
+	playIntv,
+	playIdx = 0,
+	intervalDuration = 50;
 
-	publicAPI.record = function(obj) {
-		console.log("starting recording");
+	publicAPI.setTarget = function (obj) {
 		motionTarget = obj;
-		interval = setInterval(recordMotion, 50);
+	};
+
+	publicAPI.record = function() {
+		console.log("starting recording");
+		recordIntv = setInterval(recordMotion, intervalDuration);
 	};
 
 	publicAPI.stopRecording = function() {
 		console.log("stopping recording");
-		clearInterval(interval);
+		clearInterval(recordIntv);
+	};
+
+	publicAPI.playRecording = function() {
+		console.log("playing recording");
+		// playIdx = 0;
+		playIntv = setInterval(playMotion, intervalDuration);
+	};
+
+	publicAPI.stopPlayback = function() {
+		console.log("stopping playback");
+		clearInterval(playIntv);
 	};
 
 	// private methods
 
 	function recordMotion () {
 		console.log("adding motion point");
-		// motion.push(motionTarget.transform);
+		motion.push(motionTarget.transform().localMatrix);
+		console.log (motion[motion.length-1]);
+	}
+
+	function playMotion () {
+		console.log("playMotion: " + motion[playIdx]);
+		motionTarget.transform(motion[playIdx]);
+		playIdx ++;
+		if (playIdx > motion.length) {
+			clearInterval(playIntv);
+			playIdx = 0;
+		}
 	}
 
 	return publicAPI;
